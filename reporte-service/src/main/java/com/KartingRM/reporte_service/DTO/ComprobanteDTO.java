d6d78140ae1556fbdf1ab1779c324580ca901d0c
Ignacio Celis Castro <ignacio.celis@usach.cl>
2025-05-24 16:42:55 -0400

    Actualización

--- reporte-service/src/main/java/com/KartingRM/reporte_service/DTO/ComprobanteDTO.java
+++ reporte-service/src/main/java/com/KartingRM/reporte_service/DTO/ComprobanteDTO.java
@@ -1,9 +1,6 @@
 package com.KartingRM.reporte_service.DTO;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.*;
 
 @Data
