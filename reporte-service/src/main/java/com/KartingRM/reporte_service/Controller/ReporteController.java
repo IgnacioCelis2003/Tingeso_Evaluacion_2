4ec4c2e36eef91e5319e211b1b6d93848a91da14
Ignacio Celis Castro <ignacio.celis@usach.cl>
2025-06-04 01:55:34 -0400

    Commit casi final

--- reporte-service/src/main/java/com/KartingRM/reporte_service/Controller/ReporteController.java
+++ reporte-service/src/main/java/com/KartingRM/reporte_service/Controller/ReporteController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/reporte")
 public class ReporteController {
 
